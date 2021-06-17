4e57b552fc38439323e70bfdce9de35d55816293
diaz-L <lediar.a.diaz@gmail.com>
2021-06-17 12:35:54 -0400

    --update gitignore to ignore .vscode directory --updated solution file to include ex11 --completed ex11: create a function that takes a string and returns the word count

---
+++ ex11/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+/*
+ * Create a function that takes a string and returns the number of words. The string will be a sentence.
+ */
+namespace ex11
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string s1 = "Just an example here, move along.",
+             s2 = "This is a test",
+             s3 = " ";
+
+      Console.WriteLine($"[{s1}] => count: {WordCount(s1)}");
+      Console.WriteLine($"[{s2}] => count: {WordCount(s2)}");
+      Console.WriteLine($"[{s3}] => count: {WordCount(s3)}");
+    }
+
+    static int WordCount(string sentence)
+    {
+      return sentence
+                .Split(new char[] { ' ', '\t', '\n' })
+                .Where(s => !string.IsNullOrWhiteSpace(s))
+                .Count();
+    }
+  }
+}
