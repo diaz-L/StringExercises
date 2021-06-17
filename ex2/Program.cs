6c6b90098397d723acb5440a2da28ac8ab7f90e4
diaz-L <43645098+diaz-L@users.noreply.github.com>
2021-06-16 22:25:48 -0400

    Update Program.cs
    remove extra spaces

--- ex2/Program.cs
+++ ex2/Program.cs
@@ -10,22 +10,22 @@ namespace ex2
     {
         static void Main(string[] args)
         {
-				string s1 = "first", s2 = "second", s3 = "third", space = " ", wildcard = "*";
+		string s1 = "first", s2 = "second", s3 = "third", space = " ", wildcard = "*";
 
-				Console.WriteLine(Concat(s1, space, s2));
-				Console.WriteLine(Concat(s1, space, s2, space, s3));
-				Console.WriteLine(Concat(space, space, s1, space, s2));
-				Console.WriteLine(Concat(wildcard, s1, space, s2, space, s3, wildcard));
+		Console.WriteLine(Concat(s1, space, s2));
+		Console.WriteLine(Concat(s1, space, s2, space, s3));
+		Console.WriteLine(Concat(space, space, s1, space, s2));
+		Console.WriteLine(Concat(wildcard, s1, space, s2, space, s3, wildcard));
         }
 
-			static string Concat(params string[] args)
-			{
-				StringBuilder sb = new StringBuilder();
+	static string Concat(params string[] args)
+	{
+		StringBuilder sb = new StringBuilder();
 
-				foreach (var arg in args)
-					sb.Append(arg);
+		foreach (var arg in args)
+			sb.Append(arg);
 
-				return sb.ToString();					
-			}
+		return sb.ToString();					
+	}
     }
 }
