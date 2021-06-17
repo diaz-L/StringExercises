6ee80ad84a35358035c687b7dca07c5ba81dcbc5
diaz-L <lediar.a.diaz@gmail.com>
2021-06-17 00:40:15 -0400

    --updated solution file to include exercise ex5 --completed ex5: write a function that returns a boolean if the string contains any whitespace

---
+++ ex5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+/*
+ * create a function that returns true if a string contains whitespaces
+ */
+namespace ex5
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string s1 = "ehlo, cruel world!!", s2 = "abc", s3 = "\tgreetings\t", s4 = string.Empty;
+
+      Console.WriteLine($">>{s1}<< contains whitespace: {HasWhitespace(s1)}");
+      Console.WriteLine($">>{s2}<< contains whitespace: {HasWhitespace(s2)}");
+      Console.WriteLine($">>{s3}<< contains whitespace: {HasWhitespace(s3)}");
+      Console.WriteLine($">>{s4}<< contains whitespace: {HasWhitespace(s4)}");
+    }
+
+    static bool HasWhitespace(string str)
+    {
+      foreach (var c in str)
+      {
+        if (char.IsWhiteSpace(c)) return true;
+      }
+
+      return false;
+    }
+  }
+}
