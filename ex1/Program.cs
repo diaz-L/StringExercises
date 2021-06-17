7ba2df1898162d60ad8d233f153376ca4c01c5f3
diaz-L <lediar.a.diaz@gmail.com>
2021-06-16 20:44:14 -0400

    -- updated solution file to include ex1 -- completed ex1: create a function that returns boolean if string is empty

---
+++ ex1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+/*
+ * Create a function that returns 'true' if a string is empty and 'false' otherwise
+ */
+namespace ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+				string s1 = "ehlo cruel world!", s2 = string.Empty, s3 = "We are the champions";
+
+				Console.WriteLine($"string s1 is empty: {IsEmpty(s1)}");
+				Console.WriteLine($"string s2 is empty: {IsEmpty(s2)}");
+				Console.WriteLine($"string s3 is empty: {IsEmpty(s3)}");
+        }
+
+			static bool IsEmpty(string text)
+			{
+				return text.Length > 0 ? true : false;
+			}
+    }
+}
