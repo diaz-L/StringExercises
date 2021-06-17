bb076839f134427401912e4e288bccc13b9270f8
diaz-L <lediar.a.diaz@gmail.com>
2021-06-16 22:21:49 -0400

    --update solution file to include ex3 --completed ex3: write recursive function that returns the length of a string

---
+++ ex3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+/*
+ * Write a function that returns the length of a string. Make your function recursive.
+ */
+namespace ex3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+				string s1 = "ehlo cruel world", s2 = "goodbye";
+				
+				Console.WriteLine($"s1 length: {Length(s1)}");
+				Console.WriteLine($"s2 length: {Length(s2)}");
+        }
+
+			static int Length(string str)
+			{
+				if (str.Length == 0) return 0;
+
+				return 1 + Length(str.Substring(1));
+			}
+    }
+}
