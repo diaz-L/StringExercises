1f47fc649fa3f159ace410421adb91895906beb5
diaz-L <lediar.a.diaz@gmail.com>
2021-06-16 22:56:19 -0400

    --updated solution to include ex4 --completed ex4: write a function that converts a string to an integer and throws exception if string doesn't start with a digiti

---
+++ ex4/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+/*
+ * Write a function that takes a string and returns it as an integer
+ */
+namespace ex4
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string num1 = "17", num2 = "27.99", num3 = "", num4 = "abc";
+
+      Console.WriteLine(ToInt(num1));
+      Console.WriteLine(ToInt(num2));
+      try
+      {
+        Console.WriteLine(ToInt(num3));
+        Console.WriteLine(ToInt(num4));
+      }
+      catch (NotANumberException e)
+      {
+        Console.WriteLine("Failed to convert string to integer");
+        Console.WriteLine(e.ToString());
+      }
+
+    }
+
+    static int ToInt(string num)
+    {
+      int length;
+      for (length = 0; length < num.Length; length++)
+      {
+        if (!char.IsDigit(num[length]))
+          break;
+      }
+
+      if (length == 0)
+        throw new NotANumberException();
+      else
+        return int.Parse(num.Substring(0, length));
+    }
+  }
+
+  public class NotANumberException : Exception { }
+}
