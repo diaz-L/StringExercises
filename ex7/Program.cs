aa4f7d21ca1a86898d5dd7c01b0af2dc347fe89f
diaz-L <lediar.a.diaz@gmail.com>
2021-06-17 02:14:58 -0400

    --updates solution file to include ex7 --completed ex7: write a function the checks if two string are equal (ignoring case)

---
+++ ex7/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Security.Cryptography;
+
+/*
+ * Write a function that validates whether two strings are identical. Make it case insensitive.
+ */
+namespace ex7
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string s1 = "Say goodnight to the bad guy..",
+              s2 = "Say goodnight to the bad guy..",
+              s3 = "goodnight to the bad guy";
+
+      var result1 = EqualIgnoreCase(s1, s2);
+      var result2 = EqualIgnoreCase(s1, s3);
+      var result3 = EqualIgnoreCase(s2, s3);
+
+      Console.WriteLine($"{s1} == {s2} [{result1}]");
+      Console.WriteLine($"{s1} == {s3} [{result2}]");
+      Console.WriteLine($"{s2} == {s3} [{result3}]");
+    }
+
+    static bool EqualIgnoreCase(string s1, string s2)
+    {
+      return string.Compare(s1, s2, true) == 0 ? true : false;
+    }
+  }
+}
