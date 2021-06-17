14edcf97ccc21cf9d135f5caf9a26875bea39c31
diaz-L <lediar.a.diaz@gmail.com>
2021-06-17 10:47:05 -0400

    --updated solution file to include ex8 --completed ex8: create a function that returns a boolean with given two string the first ends with the second

---
+++ ex8/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+/*
+ * Create a function that takes two string and returns true if the first string ends with second string
+ */
+namespace ex8
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string s1 = "abc", s2 = "samurai", s3 = "feminine";
+
+      Console.WriteLine(EndsWith(s1, "bc"));
+      Console.WriteLine(EndsWith(s1, "d"));
+      Console.WriteLine(EndsWith(s2, "zi"));
+      Console.WriteLine(EndsWith(s3, "nine"));
+    }
+
+    static bool EndsWith(string s1, string s2)
+    {
+      return s1.EndsWith(s2);
+    }
+  }
+}
