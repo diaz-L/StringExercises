0fd451ea95e2407812196d04da45370391a31e39
diaz-L <lediar.a.diaz@gmail.com>
2021-06-17 11:51:58 -0400

    --updated solution file to include ex10 --completed ex10: create a function that's given an array, string and returns the index of given string

---
+++ ex10/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text;
+
+/*
+ * create a function that takes an array and string and returns the index of string 
+ */
+namespace ex10
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string[] arr1 = { "hi", "edabit", "fgh", "abc" },
+               arr2 = { "Red", "blue", "Blue", "Green" },
+               arr3 = { "a", "g", "y", "d" },
+               arr4 = { "Pineapple", "Orange", "Grape", "Apple" };
+
+      string s1 = "fgh", s2 = "blue", s3 = "d", s4 = "Pineapple", s5 = "abc";
+
+      Console.WriteLine($"array: {Print(arr1)}, target: {s1}, index: {IndexOf(arr1, s1)}");
+      Console.WriteLine($"array: {Print(arr2)}, target: {s2}, index: {IndexOf(arr2, s2)}");
+      Console.WriteLine($"array: {Print(arr3)}, target: {s3}, index: {IndexOf(arr3, s3)}");
+      Console.WriteLine($"array: {Print(arr4)}, target: {s4}, index: {IndexOf(arr4, s4)}");
+      Console.WriteLine($"array: {Print(arr3)}, target: {s5}, index: {IndexOf(arr3, s5)}");
+    }
+
+    static int IndexOf(string[] arr, string s1)
+    {
+      for (var i = 0; i < arr.Length; i++)
+        if (arr[i] == s1) return i;
+
+      return -1;
+    }
+
+    static string Print(string[] list)
+    {
+      StringBuilder output = new StringBuilder();
+
+      output.Append("[ ");
+      foreach (var item in list)
+        output.Append($"{item} ");
+      output.Append("]");
+
+      return output.ToString();
+    }
+  }
+}
