92d972f00e89929d03349bebb171f25de46a565e
diaz-L <43645098+diaz-L@users.noreply.github.com>
2021-06-16 22:27:26 -0400

    Update Program.cs
    remove extra spaces

--- ex1/Program.cs
+++ ex1/Program.cs
@@ -9,16 +9,16 @@ namespace ex1
     {
         static void Main(string[] args)
         {
-				string s1 = "ehlo cruel world!", s2 = string.Empty, s3 = "We are the champions";
+		string s1 = "ehlo cruel world!", s2 = string.Empty, s3 = "We are the champions";
 
-				Console.WriteLine($"string s1 is empty: {IsEmpty(s1)}");
-				Console.WriteLine($"string s2 is empty: {IsEmpty(s2)}");
-				Console.WriteLine($"string s3 is empty: {IsEmpty(s3)}");
+		Console.WriteLine($"string s1 is empty: {IsEmpty(s1)}");
+		Console.WriteLine($"string s2 is empty: {IsEmpty(s2)}");
+		Console.WriteLine($"string s3 is empty: {IsEmpty(s3)}");
         }
 
-			static bool IsEmpty(string text)
-			{
-				return text.Length > 0 ? true : false;
-			}
+	static bool IsEmpty(string text)
+	{
+		return text.Length > 0 ? true : false;
+	}
     }
 }
