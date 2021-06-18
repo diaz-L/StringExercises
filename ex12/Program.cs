a9541f81308702d86c7dabb6f8d71696467216ea
diaz-L <lediar.a.diaz@gmail.com>
2021-06-17 21:10:42 -0400

    --completed ex12: create a function that given a string and character returns number of occurrences of char in string --updated solution file to include ex12

---
+++ ex12/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+
+/*
+ * Create a function that takes a character and string and returns the number of times the 
+ * character is found in the string.
+ */
+namespace ex12
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string s1 = "edabit", s2 = "Chamber of secrets", s3 = "big fat bubble";
+      char c1 = 'a', c2 = 'c', c3 = 'b';
+
+      Console.WriteLine($"{s1} >>{c1}<< count: {CharCount(c1, s1)}");
+      Console.WriteLine($"{s2} >>{c2}<< count: {CharCount(c2, s2)}");
+      Console.WriteLine($"{s3} >>{c3}<< count: {CharCount(c3, s3)}");
+    }
+
+    static int CharCount(char c, string s1)
+    {
+      int count = 0;
+      foreach (var letter in s1)
+        if (c == letter)
+          count++;
+
+      return count;
+    }
+  }
+}
