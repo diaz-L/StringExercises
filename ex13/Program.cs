26d41d6f3b1a948c3912882cef9d851865e56f17
diaz-L <lediar.a.diaz@gmail.com>
2021-06-21 22:53:28 -0400

    --completed ex13: write a function that takes a string and return number of vowels --edited solution file to include ex13

---
+++ ex13/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+/*
+ * Write a function that given a string returns the number of vowels contained within it
+ */
+namespace ex13
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string s1 = "Celebration", s2 = "Palm", s3 = "Prediction";
+
+      PrintVowelCount(s1);
+      PrintVowelCount(s2);
+      PrintVowelCount(s3);
+    }
+
+    static void PrintVowelCount(string s1)
+    {
+      Console.WriteLine($"string: {s1}, count: {CountVowels(s1)}");
+    }
+
+    static int CountVowels(string s1)
+    {
+      int count = 0;
+
+      foreach (var c in s1)
+        switch (c)
+        {
+          case 'a':
+          case 'A':
+          case 'e':
+          case 'E':
+          case 'i':
+          case 'I':
+          case 'o':
+          case 'O':
+          case 'u':
+          case 'U':
+            count++;
+            break;
+        }
+
+      return count;
+    }
+  }
+}
