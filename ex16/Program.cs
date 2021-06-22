dc25bd5ee9333fc7d13277fff7dcbe0cbb4fd4ca
diaz-L <lediar.a.diaz@gmail.com>
2021-06-21 23:46:40 -0400

    --completed ex16: create a function that takes a string and returns a string with spaces between characters --edited solution file to include ex16

---
+++ ex16/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+/*
+ * create a function that takes a string and returns a string with space between characters
+ */
+namespace ex16
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string s1 = "space", s2 = "far out", s3 = "elongated musk";
+
+      PrintSpaceOut(s1);
+      PrintSpaceOut(s2);
+      PrintSpaceOut(s3);
+    }
+
+    static void PrintSpaceOut(string s1)
+    {
+      Console.WriteLine($"original: `{s1}`");
+      Console.WriteLine($"space_out: `{SpaceMeOut(s1)}`");
+      Console.WriteLine();
+    }
+
+    static string SpaceMeOut(string s1)
+    {
+      char[] charList = s1.ToCharArray();
+
+      return string.Join(' ', charList);
+    }
+  }
+}
