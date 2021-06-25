621f29717542821f897fbf5a037b8b921b091561
diaz-L <lediar.a.diaz@gmail.com>
2021-06-24 23:28:24 -0400

    --completed ex19: create a function that takes a string and repeats the last character 'n' number of times   ArgumentException thrown for 'null' or empty string --updated solution file to include ex19

---
+++ ex19/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+/*
+ * create a function that takes a string and repeats the last character 'n' number of times
+ */
+namespace ex19
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string s1 = "hello", s2 = "hey", s3 = "execuse me, what?";
+
+      PrintModifyString(s1, 3);
+      PrintModifyString(s2, 6);
+      PrintModifyString(s3, 5);
+
+      try
+      {
+        PrintModifyString("", 2);
+        PrintModifyString(null, 2);
+      }
+      catch (Exception)
+      {
+        Console.WriteLine(">>exception caught for empty or null<<");
+      }
+    }
+
+    static void PrintModifyString(string s1, int n)
+    {
+      Console.WriteLine($"`{s1}` -> `{ModifyLast(s1, n)}`");
+    }
+
+    static string ModifyLast(string s1, int n)
+    {
+      if (string.IsNullOrEmpty(s1))
+        throw new ArgumentException("string null or empty");
+
+      int lastIndex = s1.Length - 1;
+
+      return s1.Substring(0, lastIndex) + new String(s1[lastIndex], n);
+    }
+  }
+}
