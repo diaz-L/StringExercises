eb701ce61504ca1721205711d67c7e72342f912a
diaz-L <lediar.a.diaz@gmail.com>
2021-06-26 22:35:16 -0400

    --completed ex26: create a function that takes two strings and returns either true or false depending on whether they're   anagrams or not. Should be case insensitive. --edited solution file to include ex26 --modified exercises file to include exercise 26

---
+++ ex26/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+/*
+ * create a function that takes two strings and returns either true or false depending on whether they're
+ * anagrams or not. Should be case insensitive.
+ */
+namespace ex26
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      PrintIsAnagram("cristian", "Cristina");
+      PrintIsAnagram("Dave Barry", "Ray Adverb");
+      PrintIsAnagram("Nope", "Note");
+      PrintIsAnagram("Apple", "Appeal");
+    }
+
+    static void PrintIsAnagram(string s1, string s2)
+    {
+      Console.WriteLine($"({s1}, {s2}) is anagram: {IsAnagram(s1, s2)}");
+    }
+
+    static bool IsAnagram(string s1, string s2)
+    {
+      var list1 = new List<char>();
+      var list2 = new List<char>();
+
+      foreach (var c in s1.ToLower())
+      {
+        if (!char.IsLetter(c)) continue;
+        list1.Add(c);
+      }
+
+      foreach (var c in s2.ToLower())
+      {
+        if (!char.IsLetter(c)) continue;
+        list2.Add(c);
+      }
+
+      list1.Sort();
+      list2.Sort();
+
+      var results1 = new String(list1.ToArray());
+      var results2 = new String(list2.ToArray());
+
+      return results1 == results2;
+    }
+  }
+}
